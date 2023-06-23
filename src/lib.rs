04c1419c6c27fbfb527d67b262b9dd893d063375
MrEnder0 <endermobtrey@gmail.com>
2023-06-23 17:41:15 -0500

    scorched

---
+++ src/lib.rs
@@ -0,0 +1,106 @@
+mod utils;
+
+use std::{fmt::Debug, fs::OpenOptions, io::prelude::*};
+use owo_colors::{colors::css::*, OwoColorize};
+
+pub enum LogImportance {
+    Error,
+    Warning,
+    Info,
+    Debug,
+}
+
+pub struct LogData {
+    pub importance: LogImportance,
+    pub message: String,
+}
+
+pub fn log_this(data: LogData) {
+    let formatted_time = utils::format_time::get_formatted_time(utils::format_time::TimeFormat::Ymdhms);
+
+    let file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(format!("{}.log", utils::format_time::get_formatted_time(utils::format_time::TimeFormat::Ymd)));
+
+    match data.importance {
+        LogImportance::Error => {
+            file.unwrap()
+                .write_all(format!("{} [ERROR] {}\n", formatted_time, data.message).as_bytes())
+                .unwrap();
+            println!(
+                "{} {} {}",
+                formatted_time,
+                "[ERROR]".fg::<Black>().bg::<Red>(),
+                data.message
+            );
+        }
+        LogImportance::Warning => {
+            file.unwrap()
+                .write_all(format!("{} [WARNING] {}\n", formatted_time, data.message).as_bytes())
+                .unwrap();
+            println!(
+                "{} {} {}",
+                formatted_time,
+                "[WARNING]".fg::<Black>().bg::<Yellow>(),
+                data.message
+            );
+        }
+        LogImportance::Info => {
+            file.unwrap()
+                .write_all(format!("{} [INFO] {}\n", formatted_time, data.message).as_bytes())
+                .unwrap();
+            println!(
+                "{} {} {}",
+                formatted_time,
+                "[INFO]".fg::<Black>().bg::<LightGray>(),
+                data.message
+            );
+        }
+        LogImportance::Debug => {
+            file.unwrap()
+                .write_all(format!("{} [DEBUG] {}\n", formatted_time, data.message).as_bytes())
+                .unwrap();
+            println!(
+                "{} {} {}",
+                formatted_time,
+                "[DEBUG]".fg::<Black>().bg::<LightBlue>(),
+                data.message
+            );
+        }
+    }
+}
+
+pub trait LogExpect<T, E: Debug> {
+    fn log_expect(self, importance: LogImportance, msg: &str) -> T;
+}
+
+impl<T, E: Debug> LogExpect<T, E> for Result<T, E> {
+    fn log_expect(self, importance: LogImportance, msg: &str) -> T {
+        match self {
+            Ok(val) => val,
+            Err(err) => {
+                log_this(LogData {
+                    importance,
+                    message: format!("{}: {:?}", msg, err),
+                });
+                panic!("{}: {:?}", msg, err);
+            }
+        }
+    }
+}
+
+impl<T> LogExpect<T, ()> for Option<T> {
+    fn log_expect(self, importance: LogImportance, msg: &str) -> T {
+        match self {
+            Some(val) => val,
+            None => {
+                log_this(LogData {
+                    importance,
+                    message: msg.to_string(),
+                });
+                panic!("{}", msg);
+            }
+        }
+    }
+}
---
+++ src/utils/format_time.rs
@@ -0,0 +1,49 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+pub enum TimeFormat {
+    Ymdhms,
+    Ymd,
+    HMS,
+}
+
+pub fn get_formatted_time(format: TimeFormat) -> String {
+    let now = SystemTime::now();
+    let duration_since_epoch = now.duration_since(UNIX_EPOCH).unwrap();
+    let seconds_since_epoch = duration_since_epoch.as_secs();
+
+    match format {
+        TimeFormat::Ymdhms => {
+            let formatted_time = format!(
+                //Y-m-d H:M:S
+                "{}-{}-{} {}:{}:{}",
+                seconds_since_epoch / 31536000 + 1970,
+                seconds_since_epoch / 2592000 % 12 + 1,
+                seconds_since_epoch / 86400 % 30 + 1,
+                seconds_since_epoch / 3600 % 24,
+                seconds_since_epoch / 60 % 60,
+                seconds_since_epoch % 60
+            );
+            return formatted_time;
+        }
+        TimeFormat::Ymd => {
+            let formatted_time = format!(
+                //Y-m-d
+                "{}-{}-{}",
+                seconds_since_epoch / 31536000 + 1970,
+                seconds_since_epoch / 2592000 % 12 + 1,
+                seconds_since_epoch / 86400 % 30 + 1
+            );
+            return formatted_time;
+        }
+        TimeFormat::HMS => {
+            let formatted_time = format!(
+                //H:M:S
+                "{}:{}:{}",
+                seconds_since_epoch / 3600 % 24,
+                seconds_since_epoch / 60 % 60,
+                seconds_since_epoch % 60
+            );
+            return formatted_time;
+        }
+    }
+}
