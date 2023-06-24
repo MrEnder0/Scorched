12785d2cc488afb9fc20f2a4e5eaaba0f2c5bb85
MrEnder0 <endermobtrey@gmail.com>
2023-06-24 16:49:11 -0500

    new time thing

--- src/utils/format_time.rs
+++ src/utils/format_time.rs
@@ -1,3 +1,4 @@
+use chrono::{TimeZone, Utc};
 use std::time::{SystemTime, UNIX_EPOCH};
 
 pub enum TimeFormat {
@@ -9,40 +10,30 @@ pub enum TimeFormat {
 
 pub fn get_formatted_time(format: TimeFormat) -> String {
     let now = SystemTime::now();
-    let duration_since_epoch = now.duration_since(UNIX_EPOCH).unwrap();
-    let seconds_since_epoch = duration_since_epoch.as_secs();
+    let since_the_epoch = now.duration_since(UNIX_EPOCH).expect("Time went backwards");
 
     match format {
         TimeFormat::Ymdhms => {
             let formatted_time = format!(
-                //Y-m-d H:M:S
-                "{}-{}-{} {}:{}.{}",
-                seconds_since_epoch / 31536000 + 1970,
-                (seconds_since_epoch % 31556926) / 2629743 + 1,
-                (seconds_since_epoch % 2629743) / 86400 + 1,
-                (seconds_since_epoch % 86400) / 3600 + 0,
-                (seconds_since_epoch % 3600) / 60,
-                seconds_since_epoch % 60
+                "{}",
+                Utc.timestamp(since_the_epoch.as_secs() as i64, 0)
+                    .format("%Y-%m-%d %H:%M:%S")
             );
             return formatted_time;
         }
         TimeFormat::Ymd => {
             let formatted_time = format!(
-                //Y-m-d
-                "{}-{}-{}",
-                seconds_since_epoch / 31536000 + 1970,
-                (seconds_since_epoch % 31556926) / 2629743 + 1,
-                (seconds_since_epoch % 2629743) / 86400 + 1,
+                "{}",
+                Utc.timestamp(since_the_epoch.as_secs() as i64, 0)
+                    .format("%Y-%m-%d")
             );
             return formatted_time;
         }
         TimeFormat::HMS => {
             let formatted_time = format!(
-                //H:M:S
-                "{}:{}:{}",
-                (seconds_since_epoch % 86400) / 3600 + 0,
-                (seconds_since_epoch % 3600) / 60,
-                seconds_since_epoch % 60
+                "{}",
+                Utc.timestamp(since_the_epoch.as_secs() as i64, 0)
+                    .format("%H:%M:%S")
             );
             return formatted_time;
         }
