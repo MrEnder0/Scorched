035cc853d09c068ecc92d54706bad6ac6c5a135e
MrEnder0 <72358826+MrEnder0@users.noreply.github.com>
2023-10-16 22:14:02 -0500

    code readability

--- src/utils/time_utils.rs
+++ src/utils/time_utils.rs
@@ -6,32 +6,21 @@ pub enum TimeFormat {
     Date,
     Time,
 }
+
 pub fn get_formatted_time(time_format: TimeFormat) -> String {
     let now = SystemTime::now();
     let since_the_epoch = now.duration_since(UNIX_EPOCH).expect("Time went backwards");
 
+    let time = Utc
+        .timestamp_opt(since_the_epoch.as_secs() as i64, 0)
+        .unwrap()
+        .with_timezone(&Utc)
+        .to_rfc3339();
+
     //Make use of time format and split str
     match time_format {
-        TimeFormat::DateTime => Utc
-            .timestamp_opt(since_the_epoch.as_secs() as i64, 0)
-            .unwrap()
-            .with_timezone(&Utc)
-            .to_rfc3339(),
-        TimeFormat::Date => Utc
-            .timestamp_opt(since_the_epoch.as_secs() as i64, 0)
-            .unwrap()
-            .with_timezone(&Utc)
-            .to_rfc3339()
-            .split('T')
-            .collect::<Vec<&str>>()[0]
-            .to_string(),
-        TimeFormat::Time => Utc
-            .timestamp_opt(since_the_epoch.as_secs() as i64, 0)
-            .unwrap()
-            .with_timezone(&Utc)
-            .to_rfc3339()
-            .split('T')
-            .collect::<Vec<&str>>()[1]
-            .to_string(),
+        TimeFormat::DateTime => time,
+        TimeFormat::Date => time.split('T').collect::<Vec<&str>>()[0].to_string(),
+        TimeFormat::Time => time.split('T').collect::<Vec<&str>>()[1].to_string(),
     }
 }
