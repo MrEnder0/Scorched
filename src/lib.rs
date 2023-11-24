0f7404668d4d7710fc646b683c87592550b6729b
MrEnder0 <72358826+MrEnder0@users.noreply.github.com>
2023-11-24 17:29:47 -0600

    Update dependencies and fixed log_except

--- src/lib.rs
+++ src/lib.rs
@@ -113,6 +113,102 @@ pub async fn log_this(data: LogData) {
     }
 }
 
+fn non_async_log_this(data: LogData) {
+    // Creates logs folder if it doesn't exist
+    if !std::path::Path::new("logs").exists() {
+        std::fs::create_dir("logs").unwrap();
+    }
+
+    let file = OpenOptions::new().append(true).create(true).open(format!(
+        "logs/{}.log",
+        utils::time_utils::get_formatted_time(utils::time_utils::TimeFormat::Date)
+    ));
+
+    match data.importance {
+        LogImportance::Error => {
+            file.unwrap()
+                .write_all(
+                    format!(
+                        "{} [ERROR] {}\n",
+                        utils::time_utils::get_formatted_time(
+                            utils::time_utils::TimeFormat::DateTime
+                        ),
+                        data.message
+                    )
+                    .as_bytes(),
+                )
+                .unwrap();
+            println!(
+                "{} {} {}",
+                utils::time_utils::get_formatted_time(utils::time_utils::TimeFormat::Time),
+                "[ERROR]".fg::<Black>().bg::<Red>(),
+                data.message
+            );
+        }
+        LogImportance::Warning => {
+            file.unwrap()
+                .write_all(
+                    format!(
+                        "{} [WARNING] {}\n",
+                        utils::time_utils::get_formatted_time(
+                            utils::time_utils::TimeFormat::DateTime
+                        ),
+                        data.message
+                    )
+                    .as_bytes(),
+                )
+                .unwrap();
+            println!(
+                "{} {} {}",
+                utils::time_utils::get_formatted_time(utils::time_utils::TimeFormat::Time),
+                "[WARNING]".fg::<Black>().bg::<Yellow>(),
+                data.message
+            );
+        }
+        LogImportance::Info => {
+            file.unwrap()
+                .write_all(
+                    format!(
+                        "{} [INFO] {}\n",
+                        utils::time_utils::get_formatted_time(
+                            utils::time_utils::TimeFormat::DateTime
+                        ),
+                        data.message
+                    )
+                    .as_bytes(),
+                )
+                .unwrap();
+            println!(
+                "{} {} {}",
+                utils::time_utils::get_formatted_time(utils::time_utils::TimeFormat::Time),
+                "[INFO]".fg::<Black>().bg::<LightGray>(),
+                data.message
+            );
+        }
+        // Mainly unused, but still available
+        LogImportance::Debug => {
+            file.unwrap()
+                .write_all(
+                    format!(
+                        "{} [DEBUG] {}\n",
+                        utils::time_utils::get_formatted_time(
+                            utils::time_utils::TimeFormat::DateTime
+                        ),
+                        data.message
+                    )
+                    .as_bytes(),
+                )
+                .unwrap();
+            println!(
+                "{} {} {}",
+                utils::time_utils::get_formatted_time(utils::time_utils::TimeFormat::Time),
+                "[DEBUG]".fg::<Black>().bg::<Magenta>(),
+                data.message
+            );
+        }
+    }
+}
+
 pub trait LogExpect<T, E: Debug> {
     fn log_expect(self, importance: LogImportance, msg: &str) -> T;
 }
@@ -122,7 +218,7 @@ impl<T, E: Debug> LogExpect<T, E> for Result<T, E> {
         match self {
             Ok(val) => val,
             Err(err) => {
-                log_this(LogData {
+                non_async_log_this(LogData {
                     importance,
                     message: format!("{}: {:?}", msg, err),
                 });
@@ -138,7 +234,7 @@ impl<T> LogExpect<T, ()> for Option<T> {
         match self {
             Some(val) => val,
             None => {
-                log_this(LogData {
+                non_async_log_this(LogData {
                     importance,
                     message: msg.to_string(),
                 });
@@ -153,7 +249,7 @@ impl<T> LogExpect<T, ()> for Option<T> {
 mod tests {
     #[test]
     fn set_logging() {
-        crate::log_this(crate::LogData {
+        crate::non_async_log_this(crate::LogData {
             importance: crate::LogImportance::Info,
             message: "Test".to_string(),
         });
