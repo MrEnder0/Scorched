a5d9e41f8478fbbae5d1d3c31372847a6d06f70b
MrEnder0 <72358826+MrEnder0@users.noreply.github.com>
2023-12-13 20:47:47 -0600

    test publish

--- src/utils/importance_tags.rs
+++ src/utils/importance_tags.rs
@@ -1,17 +1,33 @@
-use owo_colors::{ComboColorDisplay, colors::{CustomColor, css::*}, OwoColorize};
+use owo_colors::{
+    colors::{css::*, CustomColor},
+    ComboColorDisplay, OwoColorize,
+};
 
-pub(crate) fn error_tag() -> ComboColorDisplay<'static, CustomColor<0, 0, 0>, CustomColor<{ u8::MAX }, 0, 0>, &'static str> {
+pub(crate) fn error_tag(
+) -> ComboColorDisplay<'static, CustomColor<0, 0, 0>, CustomColor<{ u8::MAX }, 0, 0>, &'static str>
+{
     "[ERROR]".fg::<Black>().bg::<Red>()
 }
 
-pub(crate) fn warning_tag() -> ComboColorDisplay<'static, CustomColor<0, 0, 0>, CustomColor<{ u8::MAX }, { u8::MAX }, 0>, &'static str> {
+pub(crate) fn warning_tag() -> ComboColorDisplay<
+    'static,
+    CustomColor<0, 0, 0>,
+    CustomColor<{ u8::MAX }, { u8::MAX }, 0>,
+    &'static str,
+> {
     "[WARNING]".fg::<Black>().bg::<Yellow>()
 }
 
-pub(crate) fn info_tag() -> ComboColorDisplay<'static, CustomColor<0, 0, 0>, CustomColor<211, 211, 211>, &'static str> {
+pub(crate) fn info_tag(
+) -> ComboColorDisplay<'static, CustomColor<0, 0, 0>, CustomColor<211, 211, 211>, &'static str> {
     "[INFO]".fg::<Black>().bg::<LightGray>()
 }
 
-pub(crate) fn debug_tag() -> ComboColorDisplay<'static, CustomColor<0, 0, 0>, CustomColor<{ u8::MAX }, 0, { u8::MAX }>, &'static str> {
+pub(crate) fn debug_tag() -> ComboColorDisplay<
+    'static,
+    CustomColor<0, 0, 0>,
+    CustomColor<{ u8::MAX }, 0, { u8::MAX }>,
+    &'static str,
+> {
     "[DEBUG]".fg::<Black>().bg::<Magenta>()
 }
